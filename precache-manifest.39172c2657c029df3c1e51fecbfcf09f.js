3f099bb5713751e32c7d1439220297b11e21b8bc
JeanGalt1957 <matthew.f.gilbertson@gmail.com>
2021-12-06 22:50:27 -0500

    Updates

---
+++ precache-manifest.39172c2657c029df3c1e51fecbfcf09f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfd5b5e52f323b13f34a",
+    "url": "/DUsocial/static/css/main.39fbd376.chunk.css"
+  },
+  {
+    "revision": "dfd5b5e52f323b13f34a",
+    "url": "/DUsocial/static/js/main.dfd5b5e5.chunk.js"
+  },
+  {
+    "revision": "9ac6342e22d1660b2da3",
+    "url": "/DUsocial/static/css/1.57107626.chunk.css"
+  },
+  {
+    "revision": "9ac6342e22d1660b2da3",
+    "url": "/DUsocial/static/js/1.9ac6342e.chunk.js"
+  },
+  {
+    "revision": "ca932057938691db8d88",
+    "url": "/DUsocial/static/js/runtime~main.ca932057.js"
+  },
+  {
+    "revision": "508753a8838d682986ee051f62d11145",
+    "url": "/DUsocial/index.html"
+  }
+];
