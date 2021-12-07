20b405fafafc7e77b45d41ff717a64b95fbf221e
JeanGalt1957 <matthew.f.gilbertson@gmail.com>
2021-12-06 23:34:59 -0500

    Deployed to matic

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,27 @@
+const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+ // contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://polygon-rpc.com/`),
+      network_id: 137,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     },
   },
   contracts_directory: './src/contracts/',
